ac046228721bdb9b26f4e399cf4331a71fbb3e7a
pikamonvvs <pikamonvvs@naver.com>
2024-07-08 23:50:03 +0900

    .

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-from loguru import logger
+import traceback
 
 import utils.utils as utils
 from recorders.recorders import *
+from utils.utils import logutil
 
 
 def main():
-    logger.add(
-        sink="logs/log_{time:YYYY-MM-DD}.log",
-        rotation="00:00",
-        retention="3 days",
-        level="INFO",
-        encoding="utf-8",
-        format="[{time:YYYY-MM-DD HH:mm:ss}][{level}][{name}][{function}:{line}]{message}",
-    )
-    args = utils.parse_args()
-    platform = globals()[args.get("platform")]
-    platform(args).run()
+    try:
+        args = utils.parse_args()
+        platform = globals()[args.get("platform")]
+        platform(args).run()
+    except Exception as ex:
+        logutil.error("Exception caught in main:")
+        logutil.error(f"{ex}\n")
+        traceback.print_exc()
 
 
 if __name__ == "__main__":
--- recorders/recorders.py
+++ recorders/recorders.py
@@ -11,7 +11,8 @@ import httpx
 import requests
 import streamlink
 from httpx_socks import AsyncProxyTransport
-from loguru import logger
+
+from utils.utils import logutil
 
 DEFAULT_INTERVAL = 10
 DEFAULT_HEADERS = {"User-Agent": "Chrome"}
@@ -38,24 +39,24 @@ class Chzzk:
         self.get_cookies()
         self.client = self.get_client()
 
-        logger.debug(f"platform: {self.platform}")
-        logger.debug(f"name: {self.name}")
-        logger.debug(f"flag: {self.flag}")
+        logutil.debug(f"platform: {self.platform}")
+        logutil.debug(f"name: {self.name}")
+        logutil.debug(f"flag: {self.flag}")
 
     def get_id_from_name(self, name):
         try:
-            logger.debug(f"Searching for channel: {name}")
+            logutil.debug(f"Searching for channel: {name}")
             response = requests.get(f"https://api.chzzk.naver.com/service/v1/search/channels?keyword={name}&size=10", headers=self.headers)
             if response.status_code == 404:
-                logger.error(f"Page not found: {response.url}")
+                logutil.error(f"Page not found: {response.url}")
                 return None
 
             response_json = response.json()
-            # logger.debug(f"response_json: {json.dumps(response_json, indent=4, ensure_ascii=False)}")
+            # logutil.debug(self.flag, f"response_json: {json.dumps(response_json, indent=4, ensure_ascii=False)}")
 
             data = response_json["content"]["data"]
             if not data:
-                logger.error(f"Cannot find channel {name}.")
+                logutil.error(f"Cannot find channel {name}.")
                 return None
 
             for channel in data:
@@ -63,15 +64,15 @@ class Chzzk:
                 if channel_name == name:
                     channel_id = channel["channel"]["channelId"]
                     if not channel_id:
-                        logger.error("Cannot find channel ID.")
+                        logutil.error("Cannot find channel ID.")
                         return None
                     return channel_id
 
         except Exception as e:
-            logger.error(f"Error occurred while fetching channel information: {e}")
+            logutil.error(f"Error occurred while fetching channel information: {e}")
             return None
 
-        logger.error(f"Cannot find channel {name}.")
+        logutil.error(f"Cannot find channel {name}.")
         return None
 
     def get_ids(self):
@@ -79,7 +80,7 @@ class Chzzk:
             channel_name = self.id
             channel_id = self.get_id_from_name(channel_name)
             if channel_id is None:
-                logger.error(f"Cannot find channel ID for name {self.id}.")
+                logutil.error(f"Cannot find channel ID for name {self.id}.")
                 return
             self.id = channel_id
             if self.name == self.id:
@@ -87,7 +88,7 @@ class Chzzk:
         else:
             channel_name, _ = self.get_channel_info(self.id)
             if channel_name is None:
-                logger.error(f"Cannot find channel name for ID {self.id}.")
+                logutil.error(f"Cannot find channel name for ID {self.id}.")
                 return
             self.name = channel_name
 
@@ -112,14 +113,14 @@ class Chzzk:
         cookies_file_path = os.path.join(plugins_dir, "cookies.txt")
 
         if not os.path.isfile(cookies_file_path) or os.path.getsize(cookies_file_path) == 0:
-            logger.warning("The cookies.txt file does not exist or is empty. Please enter the cookie values.")
-            logger.warning("\nReference: https://github.com/BlackOut-git/Chzzk-live-recorder")
+            logutil.warning("The cookies.txt file does not exist or is empty. Please enter the cookie values.")
+            logutil.warning("\nReference: https://github.com/BlackOut-git/Chzzk-live-recorder")
             NID_AUT = input("Enter the NID_AUT cookie value: ")
             NID_SES = input("Enter the NID_SES cookie value: ")
             with open(cookies_file_path, "w") as f:
                 f.write(f"NID_AUT={NID_AUT}; NID_SES={NID_SES};")
         else:
-            logger.info(f"cookies.txt exists on {cookies_file_path}")
+            logutil.info(f"cookies.txt exists on {cookies_file_path}")
 
         with open(cookies_file_path, "r") as f:
             return f.read().strip()
@@ -158,35 +159,32 @@ class Chzzk:
         try:
             response = requests.get(f"https://api.chzzk.naver.com/service/v2/channels/{channel_id}/live-detail", headers=self.headers)
             if response.status_code == 404:
-                logger.error(f"Page not found: {response.url}")
+                logutil.error(self.flag, f"Page not found: {response.url}")
                 return None
 
             response_json = response.json()
-            # logger.debug(f"response_json: {json.dumps(response_json, indent=4, ensure_ascii=False)}")
+            # logutil.debug(self.flag, f"response_json: {json.dumps(response_json, indent=4, ensure_ascii=False)}")
 
             content = response_json["content"]
             if not content:
-                logger.error("Cannot find channel information.")
+                logutil.error(self.flag, "Cannot find channel information.")
                 return None
 
             channel_name = content["channel"]["channelName"]
             if not channel_name:
-                logger.error("Cannot find channel name.")
+                logutil.error(self.flag, "Cannot find channel name.")
                 return None
 
             title = content["liveTitle"].rstrip()
             if not title:
-                logger.error("Cannot find title.")
+                logutil.error(self.flag, "Cannot find title.")
                 return None
 
             return channel_name, title
 
         except Exception as e:
-            logger.error(f"Error occurred while fetching channel information: {e}")
+            logutil.error(self.flag, f"Error occurred while fetching channel information: {e}")
             return None
-
-        logger.error("Cannot find channel information.")
-        return None
 
     def get_status(self, channel_id):
         try:
@@ -195,17 +193,17 @@ class Chzzk:
                 return None
 
             response_json = response.json()
-            # logger.debug(f"response_json: {json.dumps(response_json, indent=4, ensure_ascii=False)}")
+            # logutil.debug(self.flag, f"response_json: {json.dumps(response_json, indent=4, ensure_ascii=False)}")
 
             status = response_json["content"]["status"]
             if not status:
-                logger.error("Cannot find channel status.")
+                logutil.error(self.flag, "Cannot find channel status.")
                 return None
 
             return status
 
         except Exception as e:
-            logger.info(f"Error occurred while fetching channel information: {e}")
+            logutil.info(self.flag, f"Error occurred while fetching channel information: {e}")
             return None
 
         return None
@@ -227,10 +225,10 @@ class Chzzk:
         for half, full in char_dict.items():
             title = title.replace(half, full)
 
-        logger.debug(f"live_time: {live_time}")
-        logger.debug(f"flag: {self.flag}")
-        logger.debug(f"title: {title}")
-        logger.debug(f"format: {format}")
+        logutil.debug(self.flag, f"live_time: {live_time}")
+        logutil.debug(self.flag, f"flag: {self.flag}")
+        logutil.debug(self.flag, f"title: {title}")
+        logutil.debug(self.flag, f"format: {format}")
 
         filename = f"[{live_time}]{self.flag}{title[:50]}.{format}"
         return filename
@@ -239,30 +237,30 @@ class Chzzk:
         base, _ = os.path.splitext(file_path)
         file_path_mp4 = f"{base}.mp4"
 
-        logger.info(f"Converting {file_path} to MP4...")
+        logutil.info(self.flag, f"Converting {file_path} to MP4...")
         try:
             # Convert the file with copying codecs
             (ffmpeg.input(file_path).output(file_path_mp4, format="mp4", vcodec="copy", acodec="copy").run(overwrite_output=True, quiet=True))
-            logger.info(f"Converted {file_path} to {file_path_mp4}")
+            logutil.info(self.flag, f"Converted {file_path} to {file_path_mp4}")
             os.remove(file_path)
         except ffmpeg.Error as e:
-            logger.info(f"Error: {e.stderr.decode('utf-8')}")
+            logutil.info(self.flag, f"Error: {e.stderr.decode('utf-8')}")
             os.remove(file_path_mp4)
             return
 
-        logger.info(f"Conversion successful: {file_path} -> {file_path_mp4}")
+        logutil.info(self.flag, f"Conversion successful: {file_path} -> {file_path_mp4}")
 
     def download_stream(self, channel_id, output_file):
         url = f"https://chzzk.naver.com/live/{channel_id}"
         stream = self.get_streamlink().streams(url).get("best")  # HLSStream[mpegts]
 
         if not stream:
-            logger.error("Cannot find any streams.")
+            logutil.error(self.flag, "Cannot find any streams.")
             return
 
         with stream.open() as fd:
             process = ffmpeg.input("pipe:0").output(output_file, vcodec="copy", acodec="copy").run_async(pipe_stdin=True, overwrite_output=True)
-            logger.info(f"Recording to {output_file}...")
+            logutil.info(self.flag, f"Recording to {output_file}...")
             try:
                 while True:
                     data = fd.read(1024)
@@ -270,7 +268,7 @@ class Chzzk:
                         break
                     process.stdin.write(data)
             except KeyboardInterrupt:
-                logger.warning("KeyboardInterrupt received. Stopping the recording...")
+                logutil.warning(self.flag, "KeyboardInterrupt received. Stopping the recording...")
                 raise
             finally:
                 process.stdin.close()
@@ -278,21 +276,21 @@ class Chzzk:
                 # self.auto_convert_mp4(output_file)
 
     def print_info(self):
-        logger.info("=============================")
-        logger.info(f"platform: {self.platform}")
-        logger.info(f"id: {self.id}")
-        logger.info(f"name: {self.name}")
-        logger.info(f"interval: {self.interval}")
-        logger.info(f"headers: {self.headers}")
-        logger.info(f"cookies: {self.cookies}")
-        logger.info(f"format: {self.format}")
-        logger.info(f"proxy: {self.proxy}")
-        logger.info(f"output: {self.output}")
-        logger.info("=============================")
+        logutil.info(self.flag, "=============================")
+        logutil.info(self.flag, f"platform: {self.platform}")
+        logutil.info(self.flag, f"id: {self.id}")
+        logutil.info(self.flag, f"name: {self.name}")
+        logutil.info(self.flag, f"interval: {self.interval}")
+        logutil.info(self.flag, f"headers: {self.headers}")
+        logutil.info(self.flag, f"cookies: {self.cookies}")
+        logutil.info(self.flag, f"format: {self.format}")
+        logutil.info(self.flag, f"proxy: {self.proxy}")
+        logutil.info(self.flag, f"output: {self.output}")
+        logutil.info(self.flag, "=============================")
 
     def run(self):
         if not self.id:
-            logger.error("Cannot find channel ID.")
+            logutil.error(self.flag, "Cannot find channel ID.")
             return
 
         self.print_info()
@@ -303,28 +301,28 @@ class Chzzk:
         while True:
             try:
                 status = self.get_status(self.id)
-                logger.debug(f"status: {status}")
+                logutil.debug(self.flag, f"status: {status}")
                 if status == "OPEN":
-                    logger.info("The channel is on air.")
+                    logutil.info(self.flag, "The channel is on air.")
 
                     _, title = self.get_channel_info(self.id)
                     file_name = self.get_filename(self.name, title, self.format)
                     output_path = os.path.join(self.output, file_name)
 
-                    logger.debug(f"channel_name: {self.name}")
-                    logger.debug(f"title: {title}")
-                    logger.debug(f"output_path: {output_path}")
+                    logutil.debug(self.flag, f"channel_name: {self.name}")
+                    logutil.debug(self.flag, f"title: {title}")
+                    logutil.debug(self.flag, f"output_path: {output_path}")
 
                     self.download_stream(self.id, output_path)
                 else:
-                    logger.info(f"The channel is offline.")
+                    logutil.info(self.flag, "The channel is offline.")
                     time.sleep(self.interval)
             except streamlink.exceptions.PluginError as e:
-                print(f"Streamlink plugin error: {e}")
+                logutil.error(self.flag, f"Streamlink plugin error: {e}")
                 time.sleep(self.interval)
             except requests.exceptions.RequestException as e:
-                print(f"HTTP request error: {e}")
+                logutil.error(self.flag, f"HTTP request error: {e}")
                 time.sleep(self.interval)
             except KeyboardInterrupt:
-                logger.info("Stopped by keyboard interrupt.")
+                logutil.warning(self.flag, "Stopped by keyboard interrupt.")
                 sys.exit(0)
